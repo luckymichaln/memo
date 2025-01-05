1f5519af6da43ad3124b6a7ed1567a934c7f479f
Michal Nowak <michalnowak.contact@gmail.com>
2025-01-05 18:49:12 +0100

    add responsiveness

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -2,7 +2,7 @@ import styles from "./Heading.module.scss";
 
 export const Heading = () => {
   return (
-    <header className={styles.heading}>
+    <header className={styles.Heading}>
       <h1>MEMOji! 🧠</h1>
       <p>Collect the pairs of emojis and have fun!</p>
     </header>
